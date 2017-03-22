b5f568f391eb861d66054d84c7be9f7b0d30b500
Igor Gnatenko <ignatenko@redhat.com>
2017-03-22 22:36:54 +0100

    remove static functions in C entirely
    Signed-off-by: Igor Gnatenko <ignatenko@redhat.com>

--- libsolv-sys/build.rs
+++ libsolv-sys/build.rs
@@ -1,7 +1,5 @@
-extern crate gcc;
 extern crate pkg_config;
 
 fn main() {
     pkg_config::probe_library("libsolv").unwrap();
-    gcc::compile_library("libsolv-static-functions.a", &["static-functions.c"]);
 }
